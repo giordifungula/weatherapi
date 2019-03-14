518285341aba0a990565032709cd3a530f560694
giordifungula <giordifungula@gmail.com>
2019-03-14 18:56:26 +0200

    Searchable Weather App

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 let url = 'http://api.openweathermap.org/data/2.5/weather?q='
-let city = 'Cape Town';
+let cityTest = 'London';
 let apiKey = '&APPID=86a27a4898b55a8ae9484802eab3c73f';
 let metric = '&units=metric';
 // London,uk&APPID=86a27a4898b55a8ae9484802eab3c73f';
 
-let api = url + city + apiKey + metric;
+let city = document.createElement('h1');
+
+// Search City Input
+function searchBtn(){
+    // alert(1);
+    let count = 1;
+    let searchBtn = document.getElementById('searchBtn');
+    let cityInput = document.getElementById('cityEnter');
+
+    let input = cityInput.value;
+    getCity(input);
+
+    let api = url + cityTest + apiKey + metric;
 
 fetch(api)
   .then(function(response) {
@@ -18,7 +30,7 @@ fetch(api)
     let cityContainer = document.createElement('div');
     cityContainer.setAttribute('class', 'citybox');
 
-    let city = document.createElement('h1');
+    // Set class for city before make city
     city.setAttribute('class', 'head');
 
     let icon = document.createElement('img');
@@ -35,26 +47,48 @@ fetch(api)
     let getTemp = data.main.temp;
     let getWind = data.wind.speed;
     let getDescp = data.weather[0].description;
-    console.log(getDescp);
+
+    // console.log(getDescp);
     // Create Citys and assign them
-    let capeTown = city.textContent = 'Cape Town';
-    let capeTownTemp = temp.textContent += getTemp + '° C' ;
-    let capeTownWind = windSpeed.textContent += getWind + ' KM/h';
-    let capeTownDescp = desc.textContent += getDescp;
-    console.log(capeTownDescp);
+    let cityTemp = temp.textContent += getTemp + '° C' ;
+    let cityWind = windSpeed.textContent += getWind + ' KM/h';
+    let cityDescp = desc.textContent += getDescp;
+    // console.log(capeTownDescp);
 
     let iconLocation = data.weather[0].icon
     
 
     icon.src = `http://openweathermap.org/img/w/${iconLocation}.png`;
     console.log(iconLocation);
-    capeTown.textContent = 'Cape Town';
+    // capeTown.textContent = 'Cape Town';
 
+    // getCity('Durban');
+    // getCity('Joburg');
     cityContainer.appendChild(city);
     cityContainer.appendChild(temp);
     cityContainer.appendChild(windSpeed);
     cityContainer.appendChild(desc);
     cityContainer.appendChild(icon);
     content.appendChild(cityContainer);
+
+    // Loop through the Displaying content
   });
+};
+
+// Creates City Template outside the Request
+// let city = document.createElement('h1');
+
+let getCity = function(citi) {
+    // let name = citi;
+    cityTest = citi;
+    console.log(citi);
+    city.textContent = citi;
+    // getTemperature();
+}
+// Collect city
+// console.log(getCity('Cape Town'));
+
+
+
+
 
